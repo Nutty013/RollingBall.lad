af3d12eb959eec7168c0b6d9512b6c2af559e9d4
Nutty013 <nutcha.kal@ku.th>
2026-07-02 17:56:19 +0700

    Add files via upload

--- Lib/RollingBall.java
+++ Lib/RollingBall.java
@@ -5,26 +5,54 @@ import java.awt.*;
 
 public class RollingBall extends JPanel {
 
+    private int size = 50;
+
+    private int x = -1;
+    private int y = -1;
+
+    private int dx = -2;
+    private int angle = 0;
+
+    public RollingBall() {
+
+        Timer timer = new Timer(15, e -> {
+
+            if (x == -1) {
+                x = (getWidth() - size) / 2;
+                y = (getHeight() - size) / 2;
+            }
+
+            x += dx;
+
+            angle += dx;
+            angle %= 360;
+
+
+            if (x + size >= getWidth()) {
+                dx = -2;
+            }
+
+            if (x <= 0) {
+                x = 0;
+                dx = 2;
+            }
+
+            repaint();
+        });
+
+        timer.start();
+    }
+
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        int size = 50; 
-
-    
-        int x = (getWidth() - size) / 2;
-        int y = (getHeight() - size) / 2;
-
-
         g.setColor(Color.BLACK);
-        g.fillArc(x, y, size, size, 0, 180);
+        g.fillArc(x, y, size, size, angle, 180);
 
-  
         g.setColor(Color.WHITE);
-        g.fillArc(x, y, size, size, 180, 180);
-
+        g.fillArc(x, y, size, size, angle + 180, 180);
 
         g.setColor(Color.BLACK);
-        g.drawLine(x, y + size / 2, x + size, y + size / 2);
 
         g.drawOval(x, y, size, size);
     }
